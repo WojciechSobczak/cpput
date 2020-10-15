56fde5a0a7343202587821d85627f1a90bf30e97
Wojciech Sobczak <wojcienty@gmail.com>
2020-10-15 16:56:58 +0200

    signaled variable extended by timeout

--- core/include/cpput/signaled_variable.hpp
+++ core/include/cpput/signaled_variable.hpp
@@ -2,31 +2,14 @@
 #include <condition_variable>
 #include <atomic>
 #include <thread>
+#include <chrono>
 
 namespace cpput {
 
-    class signaled_variable {
+    class signaled_variable_def {
     protected:
         std::condition_variable conditional_variable{};
         std::atomic_bool signaled_state{ false };
-    public:
-        signaled_variable() = default;
-        signaled_variable(const signaled_variable& other) = delete;
-        signaled_variable(signaled_variable&& other) = delete;
-        signaled_variable& operator=(const signaled_variable& other) = delete;
-        signaled_variable& operator=(signaled_variable&& other) = delete;
-
-        void wait(std::unique_lock<std::mutex>& lock) noexcept;
-        void wait_for_signal(std::unique_lock<std::mutex>& lock) noexcept;
-
-        bool is_signaled() const noexcept;
-        void signal() noexcept;
-    };
-
-    class signaled_variable_def : public signaled_variable {
-    protected:
-        std::mutex defaultMutex{};
-        std::unique_lock<std::mutex> defaultLock{ defaultMutex };
     public:
         signaled_variable_def() = default;
         signaled_variable_def(const signaled_variable_def& other) = delete;
@@ -34,7 +17,65 @@ namespace cpput {
         signaled_variable_def& operator=(const signaled_variable_def& other) = delete;
         signaled_variable_def& operator=(signaled_variable_def&& other) = delete;
 
+        void wait(std::unique_lock<std::mutex>& lock) noexcept;
+        void wait_for_signal(std::unique_lock<std::mutex>& lock) noexcept;
+
+        template <typename Rep, typename Period>
+        bool wait_for_signal_for(std::unique_lock<std::mutex>& lock, std::chrono::duration<Rep, Period> time) noexcept;
+
+        template <typename Rep, typename Period>
+        bool wait_for(std::unique_lock<std::mutex>& lock, std::chrono::duration<Rep, Period> time) noexcept;
+
+        bool is_signaled() const noexcept;
+        void signal() noexcept;
+    };
+
+    class signaled_variable : public signaled_variable_def {
+    protected:
+        std::mutex defaultMutex{};
+        std::unique_lock<std::mutex> defaultLock{ defaultMutex };
+    public:
+        signaled_variable() = default;
+        signaled_variable(const signaled_variable& other) = delete;
+        signaled_variable(signaled_variable&& other) = delete;
+        signaled_variable& operator=(const signaled_variable& other) = delete;
+        signaled_variable& operator=(signaled_variable&& other) = delete;
+
         void wait() noexcept;
         void wait_for_signal() noexcept;
+
+        template <typename Rep, typename Period>
+        bool wait_for_signal_for(std::chrono::duration<Rep, Period> time) noexcept;
+
+        template <typename Rep, typename Period>
+        bool wait_for(std::chrono::duration<Rep, Period> time) noexcept;
     };
+
+    template <typename Rep, typename Period>
+    bool signaled_variable_def::wait_for_signal_for(std::unique_lock<std::mutex>& lock, std::chrono::duration<Rep, Period> time) noexcept {
+        bool expected = true;
+        if (this->signaled_state.compare_exchange_strong(expected, false)) {
+            return;
+        }
+        this->conditional_variable.wait_for(lock, time);
+        this->signaled_state.store(false);
+    }
+
+    template <typename Rep, typename Period>
+    bool signaled_variable_def::wait_for(std::unique_lock<std::mutex>& lock, std::chrono::duration<Rep, Period> time) noexcept {
+        this->conditional_variable.wait_for(lock, time);
+        this->signaled_state.store(false);
+    }
+
+    template <typename Rep, typename Period>
+    bool signaled_variable::wait_for_signal_for(std::chrono::duration<Rep, Period> time) noexcept {
+        signaled_variable::wait_for_signal_for(this->defaultLock, time);
+    }
+
+    template <typename Rep, typename Period>
+    bool signaled_variable::wait_for(std::chrono::duration<Rep, Period> time) noexcept {
+        signaled_variable::wait_for(this->defaultLock, time);
+    }
+
 };
+
--- core/src/signaled_variable.cpp
+++ core/src/signaled_variable.cpp
@@ -2,20 +2,20 @@
 
 namespace cpput {
 
-    void signaled_variable_def::wait() noexcept {
-        signaled_variable::wait(this->defaultLock);
+    void signaled_variable::wait() noexcept {
+        signaled_variable_def::wait(this->defaultLock);
     }
 
-    void signaled_variable_def::wait_for_signal() noexcept {
-        signaled_variable::wait_for_signal(this->defaultLock);
+    void signaled_variable::wait_for_signal() noexcept {
+        signaled_variable_def::wait_for_signal(this->defaultLock);
     }
 
-    void signaled_variable::wait(std::unique_lock<std::mutex>& lock) noexcept {
+    void signaled_variable_def::wait(std::unique_lock<std::mutex>& lock) noexcept {
         this->conditional_variable.wait(lock);
         this->signaled_state.store(false);
     }
 
-    void signaled_variable::wait_for_signal(std::unique_lock<std::mutex>& lock) noexcept {
+    void signaled_variable_def::wait_for_signal(std::unique_lock<std::mutex>& lock) noexcept {
         bool expected = true;
         if (this->signaled_state.compare_exchange_strong(expected, false)) {
             return;
@@ -24,11 +24,11 @@ namespace cpput {
         this->signaled_state.store(false);
     }
 
-    bool signaled_variable::is_signaled() const noexcept {
+    bool signaled_variable_def::is_signaled() const noexcept {
         return this->signaled_state.load();
     }
 
-    void signaled_variable::signal() noexcept {
+    void signaled_variable_def::signal() noexcept {
         this->signaled_state.store(true);
         this->conditional_variable.notify_all();
     }
