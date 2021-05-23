ca100a30bb89ec99dd5df2e1e1860a54444c5f01
Wojciech Sobczak <wojcienty@gmail.com>
2021-05-24 00:10:41 +0200

    to_lower_case and to_upper_case made noexcept. Made const input when possible for to_lower_case and to_upper_case.

--- include/cpput/string.hpp
+++ include/cpput/string.hpp
@@ -3,15 +3,17 @@
 #include <string>
 
 namespace cpput {
-    void to_lower_case(std::string& string);
-    void to_upper_case(std::string& string);
 
-    std::string to_lower_case_new(std::string& string);
-    std::string to_upper_case_new(std::string& string);
+    void to_lower_case(std::string& string) noexcept;
+    void to_upper_case(std::string& string) noexcept;
 
-    void to_lower_case(std::wstring& string);
-    void to_upper_case(std::wstring& string);
+    std::string to_lower_case_new(const std::string& string) noexcept;
+    std::string to_upper_case_new(const std::string& string) noexcept;
 
-    std::wstring to_lower_case_new(std::wstring& string);
-    std::wstring to_upper_case_new(std::wstring& string);
+    void to_lower_case(std::wstring& string) noexcept;
+    void to_upper_case(std::wstring& string) noexcept;
+
+    std::wstring to_lower_case_new(const std::wstring& string) noexcept;
+    std::wstring to_upper_case_new(const std::wstring& string) noexcept;
+
 }
--- src/string.cpp
+++ src/string.cpp
@@ -3,40 +3,41 @@
 #include <cwctype>
 
 namespace cpput {
-    void to_lower_case(std::string& string) {
+
+    void to_lower_case(std::string& string) noexcept {
         std::transform(string.begin(), string.end(), string.begin(), std::tolower);
     }
-    void to_upper_case(std::string& string) {
+    void to_upper_case(std::string& string) noexcept {
         std::transform(string.begin(), string.end(), string.begin(), std::toupper);
     }
 
-    std::string to_lower_case_new(std::string& string) {
+    std::string to_lower_case_new(const std::string& string) noexcept {
         std::string copy = string;
         to_lower_case(copy);
         return copy;
     }
 
-    std::string to_upper_case_new(std::string& string) {
+    std::string to_upper_case_new(const std::string& string) noexcept {
         std::string copy = string;
         to_upper_case(copy);
         return copy;
     }
 
-    void to_lower_case(std::wstring& string) {
+    void to_lower_case(std::wstring& string) noexcept {
         std::transform(string.begin(), string.end(), string.begin(), std::towlower);
     }
 
-    void to_upper_case(std::wstring& string) {
+    void to_upper_case(std::wstring& string) noexcept {
         std::transform(string.begin(), string.end(), string.begin(), std::towupper);
     }
 
-    std::wstring to_lower_case_new(std::wstring& string) {
+    std::wstring to_lower_case_new(const std::wstring& string) noexcept {
         std::wstring copy = string;
         to_lower_case(copy);
         return copy;
     }
 
-    std::wstring to_upper_case_new(std::wstring& string) {
+    std::wstring to_upper_case_new(const std::wstring& string) noexcept {
         std::wstring copy = string;
         to_upper_case(copy);
         return copy;
