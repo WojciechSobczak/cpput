0efc9fc9ad16605bb8232c43c05980577181d034
WojciechSobczak <wojcienty@gmail.com>
2025-04-14 00:31:45 +0200

    Fixed naming issues with synchronized.

--- src/synchronized.hpp
+++ src/synchronized.hpp
@@ -7,22 +7,15 @@
 namespace cpput 
 {
 
-template <typename Callable, typename ... Args>
-void synchronized(std::mutex& mutex, Callable func, Args&& ... args) {
-    const std::lock_guard lock(mutex);
-    func(std::forward<Args>(args)...);
-}
-
 template <typename resource_type>
 class synchronized {
 private:
     resource_type resource;
     std::mutex mutex;
 public:
-    synchronized() = default;
     explicit synchronized(resource_type resource) : resource(std::move(resource)) {};
 
-    void synchronized(const std::function<void(resource_type&)>& callback) {
+    void use(const std::function<void(resource_type&)>& callback) {
         std::lock_guard lock(this->mutex);
         callback(this->resource);
     }
